5cbfb45c8061ad19703872ea73de7dbd3580986f
Aaron Lewis <aaron.lewis1011@gmail.com>
2020-05-07 11:03:43 +1000

    added routes for repos and comments, added comments controller

--- githot/config/routes.rb
+++ githot/config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
-  get "/", to: "pages#index", as: "root"
+
+  get "/", to: "repos#index", as: "root"
+  post "/", to: "repos#create"
+  get "/repos/new", to: "repos#new", as: "new_repo"
+  get "/repos/:id", to: "repos#show", as: "repo"
+  put "/repos/:id", to: "repos#update"
+  patch "/repos/:id", to: "repos#update"
+  delete "/repos/:id", to: "repos#destroy"
+
+  get "/comments", to: "comments#index", as: "root"
+  post "/comments", to: "comments#create"
+  get "/comments/new", to: "comments#new", as: "new_comment"
+  get "/comments/:id", to: "comments#show", as: "comment"
+  put "/comments/:id", to: "comments#update"
+  patch "/comments/:id", to: "comments#update"
+  delete "/comments/:id", to: "comments#destroy"
+
+
+
 end
